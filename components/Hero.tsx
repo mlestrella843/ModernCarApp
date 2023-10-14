b78229641f691058c4eab8749f3c84aa62615ad3
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-10-14 07:25:16 -0600

    Creating custom buttons and reusable components

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,14 @@
+"use client";
 import React from 'react';
 import Image from 'next/image';
+import { CustomButton } from './';
 
-const Hero = () => {
+
+const Hero = ( ) => {
+
+  const handleScroll = () => {
+
+  }
   return (
     <div className='hero'>
       <div className='flex-1 pt-36 padding-x'>
@@ -12,9 +19,14 @@ const Hero = () => {
         <p className='hero__subtitle'>
             Streamline your car rental experience with our effortless 
             booking process.
-
-
         </p>
+
+        <CustomButton 
+          title = "Explore Cars"
+          containerStyles = "bg-primary-blue text-white rounded-full mt-10"
+          handleClick= { handleScroll }
+        />
+        
       </div>
     </div>
   )
