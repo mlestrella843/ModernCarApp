41e330e4af85fe6dc98f2eb7b8a146f66db424e2
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-10-17 08:29:48 -0600

    Fixing some classes

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -31,8 +31,11 @@ const CarCard = ({ car }:CarCardProps) => {
             <span className="self-end text-[14px] font-medium">
                 /day
             </span>
-
         </p>
+
+        <div className="relative w-full h-40 my-3 object-contain">
+            <Image src="/hero.png" alt="car model" fill priority className="object-contain" />
+        </div>
         
     </div>
   )
